0e86df588e44dabe8b2ff9787a464f845ec55f6b
Brandon Neth <brandonneth@Brandons-MacBook-Pro.local>
2022-01-19 17:00:44 -0700

    intro example visualizer code

--- intro-example.py
+++ intro-example.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 badtime = -1
 changetime = -1
@@ -10,6 +11,25 @@ with open('intro-example.times','r') as f:
 	changetime = float(f.readline())
 	goodtime = float(f.readline())
 
-df = pd.DataFrame([badtime,changetime,goodtime])
+data = {'BadLayout' : badtime, 'GoodLayout' : goodtime, 'LayoutChange' :changetime}
 
-print(df)
+data = {'Computation' : ['BadLayout', 'GoodLayout', 'LayoutChange'], 'Execution Time (s)' : [badtime, goodtime, changetime]}
+
+df = pd.DataFrame(data)
+
+
+
+from plotnine import ggplot, aes, geom_col
+
+g = (ggplot(df) + aes(x='Computation', y='Execution Time (s)') + geom_col())
+
+
+g.draw(show=True)
+
+g.save('IntroExampleGraph.pdf')
+
+speedup = badtime / goodtime
+print('speedup: ', speedup)
+
+print("including conversion:", badtime / (goodtime + changetime))
+
