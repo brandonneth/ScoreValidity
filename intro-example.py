4a924b336f1e0df8f34508220132ea231b29c1d4
Brandon Neth <brandonneth@Brandons-MacBook-Pro.local>
2022-01-30 16:56:34 -0700

    added stacked plot for intro example

--- intro-example.py
+++ intro-example.py
@@ -11,22 +11,27 @@ with open('intro-example.times','r') as f:
 	changetime = float(f.readline())
 	goodtime = float(f.readline())
 
-data = {'BadLayout' : badtime, 'GoodLayout' : goodtime, 'LayoutChange' :changetime}
 
-data = {'Computation' : ['BadLayout', 'GoodLayout', 'LayoutChange'], 'Execution Time (s)' : [badtime, goodtime, changetime]}
+Sequence = ['BadLayoutOnly', 'GoodLayoutOnly', 'LayoutChangeOnly', 'SwitchAndRun','SwitchAndRun']
+computation = ['BadLayout', 'GoodLayout', 'LayoutChange', 'LayoutChange','GoodLayout']
+times = [badtime, goodtime, changetime,  changetime,goodtime,]
+data = {'Computation' : computation, 'Sequence' : Sequence, 'Execution Time (s)' : times}
+
+
 
 df = pd.DataFrame(data)
-
-
+df['Computation'] = pd.Categorical(df['Computation'], categories=['BadLayout', 'LayoutChange', 'GoodLayout'])
+#df['Computation'] = pd.Categorical(df['Computation'], categories=['BadLayout', 'GoodLayout', 'LayoutChange' ])
 
 from plotnine import ggplot, aes, geom_col
 
-g = (ggplot(df) + aes(x='Computation', y='Execution Time (s)') + geom_col())
+g = (ggplot(df) + aes(x='Sequence', y='Execution Time (s)', fill='Computation') + geom_col())
 
-
-g.draw(show=True)
 
 g.save('IntroExampleGraph.pdf')
+g.draw(show=True)
+
+
 
 speedup = badtime / goodtime
 print('speedup: ', speedup)
