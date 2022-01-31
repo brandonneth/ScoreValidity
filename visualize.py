f99d42cd3e5f876187de2352022d98b88d0b9439
Brandon Neth <brandonneth@Brandons-MacBook-Pro.local>
2022-01-30 20:46:51 -0700

    removed color from the access benchmarks bc there's not enough colors for benchmark 3

--- visualize.py
+++ visualize.py
@@ -35,6 +35,7 @@ print('orders:', orders)
 
 df = pd.DataFrame(d, columns=["Access Order", "LastDigit", "Score", "Time"])
 
+
 orders = list(set(orders))
 orders.sort()
 
@@ -45,11 +46,12 @@ print(df)
 import matplotlib.pyplot as plt
 from plotnine import *
 
-p1 = ggplot(df, aes(x="Access Order", y='Time', color='Access Order')) 
+p1 = ggplot(df, aes(x="Access Order", y='Time')) 
 p1 += geom_boxplot()
-p1 += scale_color_brewer(type='qual', palette=2)
+#p1 += scale_fill_brewer(type='qual', palette='Paired')
 p1 += geom_jitter()
-
+p1 += theme(axis_text_x=element_text(rotation=-45,color='black'))
+p1 += theme(axis_text_y=element_text(color='black'))
 p1.draw(show=True)
 
 filename = sys.argv[1].split('.')[0] + "_boxplot.pdf"
