21fb4eab3fbdaada1482cee8431e9bf166259af0
brandonneth <nethbrandone@gmail.com>
2022-10-04 14:40:39 -0700

    updated visualizer to draw the points and estimate the smooth line

--- weirdness.py
+++ weirdness.py
@@ -5,9 +5,10 @@ from plotnine import *
 data = pd.read_csv(sys.argv[1])
 
 
-p = ggplot(data, aes(x='Size',y='Time',fill='Component'))
+p = ggplot(data, aes(x='Size',y='Time',color='Component', fill='Component'))
 p += facet_wrap('Variant')
-p += geom_line()
+p += geom_point()
+p += geom_smooth()
 
 #p.draw(show=True)
 
