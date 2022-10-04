5e27bf07481d9946be04c9a6fcdd6a6d39325569
Brandon Eliot Neth <neth3@lassen459.coral.llnl.gov>
2022-10-04 16:05:19 -0700

    new script and resluts

--- weirdness.sh
+++ weirdness.sh
@@ -7,7 +7,7 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 echo "Variant,Component,Time,Size" > results
 
-for EXEC in original hand; do
+for EXEC in original hand right; do
 for FACTOR in 0.5 0.7 0.8 0.9 1 1.2 1.3 1.5 1.55 1.6 1.65 1.7 2 4; do
   ./bin/2mm-$EXEC.exe $FACTOR >> results
 done
