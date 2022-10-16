8f534c2bb518bae4136f896485eb44eeed36bf8b
Brandon Eliot Neth <neth3@quartz1532.llnl.gov>
2022-10-16 15:37:56 -0700

    results for quartz

--- weirdness.sh
+++ weirdness.sh
@@ -7,8 +7,8 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 echo "Variant,Component,Time,Size,L3 Misses,L3 Accesses,L3 Miss Ratio" > results
 
-for FACTOR in 0.5 1 1.25 1.5 1.75 2 2.5; do
-for EXEC in original hand right; do
+for FACTOR in 0.5 1 1.25 1.5 1.75 2 2.1 2.2 2.3 2.4 2.5; do
+for EXEC in original hand; do
   ./bin/2mm-$EXEC.exe $FACTOR >> results
 done
 done
